3e473965345ee490f36259c3b67a52ab087f3593
Tiffany Donner <tiffany.donner@stsupport.se>
2020-01-28 10:35:05 +0000

    Other Traversing Methods - Challenge 3 Completed

--- js/script.js
+++ js/script.js
@@ -35,4 +35,11 @@ $(".theButton").mouseenter(function(){
 $(".theButton").mouseout(function(){
 	$(this).removeClass("makeBlack");
    });
+
+   //will display rgb value of selected panel in the reset panel
+ $(".theButton").click(function(){
+    var col = $(this).css('background-color');
+  $('.superButton').text( col);
+
+   });
 });
